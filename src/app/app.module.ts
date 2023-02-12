34fa5f6a789293d9d631db81b9ca6a1002013758
YaraMohammed98 <yara.mohammed.samir@gmail.com>
2023-02-12 23:55:31 +0200

    all

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './Components/navbar/navbar.component';
+import { InfoSectionComponent } from './Components/Home/info-section/info-section.component';
+import { ProductSectionComponent } from './Components/Home/product-section/product-section.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    InfoSectionComponent,
+    ProductSectionComponent
   ],
   imports: [
     BrowserModule,
